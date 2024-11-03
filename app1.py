91a209a78967ad7b332d2466fef263ad5c0ff41e
FATIMA7717 <atiquefatima6@gmail.com>
2024-11-03 13:13:19 +0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,4 +1,4 @@
-streamlit run app1.py
+
 # Step 1: Install required libraries
 # Run this command in the terminal to install Streamlit and Matplotlib if they are not already installed.
 # !pip install streamlit matplotlib numpy
